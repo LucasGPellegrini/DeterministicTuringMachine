062de76c3c72942ec6df28bc847f4e0a63649ef4
LucasGPellegrini <luskitagpell@gmail.com>
2023-04-04 12:46:34 -0300

    criei uma main, programa iterativo, defeitos especiais

---
+++ main.py
@@ -0,0 +1,74 @@
+from turingmachine import MaquinaTuring
+import os
+import sys
+
+# Constante para limpar terminal (Linux ou Windows)
+CLEAR = 'clear' if sys.platform.startswith('linux') else 'cls'
+
+def main():
+    opt = 0
+    cadeia = ""
+
+    # ----------------------------------------------------------
+    inicial = "init"
+    func_trans = {("init","0"):("init", "1", ">"),
+                       ("init","1"):("init", "0", ">"),
+                       ("init"," "):("final"," ", "*"),
+                       }
+    finais = {"final"}
+    alfa = ["0", "1"]
+    desc = "Maquina para inverter binario"
+    mt1 = MaquinaTuring(desc, cadeia, func_trans, inicial, finais)
+    # ----------------------------------------------------------
+    inicial = "q0"
+    func_trans = {("q0", "a"):("q1", "#", ">"),
+                  ("q0", "b"):("q4", "b", ">"),
+                  ("q1", "a"):("q1", "a", ">"),
+                  ("q1", "b"):("q1", "b", ">"),
+                  ("q1", " "):("q2", " ", "<"),
+                  ("q1", "#"):("q2", "#", "<"),
+                  ("q2", "a"):("q3", "#", "<"),
+                  ("q3", "a"):("q3", "a", "<"),
+                  ("q3", "b"):("q3", "b", "<"),
+                  ("q3", "#"):("q0", "#", ">"),
+                  ("q4", "#"):("qf", "#", "*")
+              }
+
+    finais = {"qf"}
+    alfa = ["a", "b"]
+    desc = "Maquina reconhecedora da linguagem L = {a^n b a^n}"
+    mt2 = MaquinaTuring(desc, alfa, func_trans, inicial, finais)
+    # ----------------------------------------------------------
+
+    while opt > 3 or opt < 1:
+        os.system(CLEAR)
+        print("--------------OPCOES--------------")
+        print("----------------------------------")
+        print("1. MT para Inverter Binario")
+        print("2. MT para verificar se uma\n   cadeia pertence a\n   L = {w | w = a^n b a^n}")
+        print("3. Sair do Programa")
+        opt = int(input("Escolha uma opção: "))
+
+        if opt == 3: return
+        else:
+            cadeia = input("Digite a cadeia: ")
+            if cadeia[-1] != " ": cadeia += " "
+            
+            if opt == 1:
+                #if not mt1.verifica_cadeia(cadeia):
+                #    print("Cadeia Invalida")
+                #    return
+                mt1.processaCadeia(cadeia)
+                main()
+            else:
+                #if not mt2.verifica_cadeia(cadeia):
+                #    print("Cadeia Invalida")
+                #    return
+                mt2.processaCadeia(cadeia)
+                main()
+
+
+
+if __name__ == "__main__":
+    main()
+
--- turingmachine.py
+++ turingmachine.py
@@ -9,7 +9,7 @@ CLEAR = 'clear' if sys.platform.startswith('linux') else 'cls'
 class MaquinaTuring:
     def __init__(self,
                  desc = "Maquina de Turing",
-                 cadeia = "", 
+                 alfabeto = "", 
                  transicoes = None, 
                  est_inicial = "", 
                  ests_finais = None):
@@ -17,11 +17,12 @@ class MaquinaTuring:
         self.simbolo_vazio = " "
         self.estado_atual = est_inicial
         self.ests_finais = set(ests_finais)
-        self.cadeia = dict(enumerate(cadeia))
         self.transicoes = transicoes
+        self.alfabeto = alfabeto
         self.aceita = False
         self.fim = False
-        self.cadeia_inicial = dict(enumerate(cadeia))
+        self.cadeia_inicial = ""
+        self.cadeia = ""
         self.descricao = desc
 
     def processa(self):
@@ -44,7 +45,9 @@ class MaquinaTuring:
                 self.aceita = True
             self.fim = True
 
-    def processaCadeia(self):
+    def processaCadeia(self, cadeia):
+        self.cadeia_inicial = dict(enumerate(cadeia))
+        self.cadeia = dict(enumerate(cadeia))
         # Cadeia Inicial
         os.system('clear')
         self.printa()
@@ -90,48 +93,3 @@ class MaquinaTuring:
                 string += str(caractere)
             return string
 
-# TESTE
-
-inicial = "init"
-func_trans = {("init","0"):("init", "1", ">"),
-                       ("init","1"):("init", "0", ">"),
-                       ("init"," "):("final"," ", "*"),
-                       }
-finais = {"final"}
-cadeia = "010011001 " 
-
-
-desc = "Maquina para inverter binario"
-t = MaquinaTuring(desc, cadeia, func_trans, inicial, finais)
-t.processaCadeia()
-
-
-# ----------------------------------------------------------
-
-inicial = "q0"
-func_trans = {("q0", "a"):("q1", "#", ">"),
-                  ("q0", "b"):("q4", "b", ">"),
-                  ("q1", "a"):("q1", "a", ">"),
-                  ("q1", "b"):("q1", "b", ">"),
-                  ("q1", " "):("q2", " ", "<"),
-                  ("q1", "#"):("q2", "#", "<"),
-                  ("q2", "a"):("q3", "#", "<"),
-                  ("q3", "a"):("q3", "a", "<"),
-                  ("q3", "b"):("q3", "b", "<"),
-                  ("q3", "#"):("q0", "#", ">"),
-                  ("q4", "#"):("qf", "#", "*")
-              }
-
-finais = {"qf"}
-cadeia = "aaabaaa "
-desc = "Maquina reconhecedora da linguagem L = {a^n b a^n}"
-t = MaquinaTuring(desc, cadeia, func_trans, inicial, finais)
-t.processaCadeia()
-
-cadeia = "aaaaabaaaa "
-t = MaquinaTuring(desc, cadeia, func_trans, inicial, finais)
-t.processaCadeia()
-
-cadeia = "aaabaaaa "
-t = MaquinaTuring(desc, cadeia, func_trans, inicial, finais)
-t.processaCadeia()
